2c384efc71392ef5f437822365976df90f517313
danielavall <dvallerine@gmail.com>
2025-05-28 10:47:13 +0700

    Tambahan page index tutor valdo

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/index', function () {
+    return view('index');
 });
 
 Auth::routes();
